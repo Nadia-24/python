7f59417c4e45fafc57e64283893fc4ab8ad03f40
nadia_alam <nadiaalam765@gmail.com>
2024-07-14 17:13:41 +0600

    add new paragraph

---
+++ basic.py
@@ -0,0 +1,13 @@
+#syntax
+print("Hello World")
+
+#variable
+a ,b , x , y , z , w = 15 , 3 , "Tanjim's age is " , 31 , 67 , "Southern"
+name = "Nadia Alam"
+print(name)
+print(x,y)
+print("sum = ",a+b)
+print("Division = ", a/b)
+#print(z + w) #error
+
+
